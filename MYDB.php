fabff792bcb74e1577726acd1ce0d0f2c3c0a37a
SunagawaDevelop <s.sunagawa1989@gmaiil.com>
2022-03-03 17:59:38 +0900

    commited

---
+++ MYDB.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<TITLE>PHPのテスト</TITLE>
+</head>
+
+<body>
+<?php
+function db_connect() {
+    $db_user = "root";
+    $db_pass = "kuwagata2022";
+    $db_host = "localhost";
+    $db_name = "sunagawa";
+    $db_type = "mysql";
+
+    $dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
+
+    try {
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+    print '接続しました。';
+
+    }catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    }
+    
+    return $pdo;
+}
+?>
+</body>
+</html>
